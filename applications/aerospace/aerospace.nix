114e7e7869f507fe2bed7d8dd9877c16e7c4b0fc
Jan Untersander <jan.untersander@ost.ch>
2025-01-09 05:29:37 +0100

    Update aerospace conf to be out of store symlink

--- applications/aerospace/aerospace.nix
+++ applications/aerospace/aerospace.nix
@@ -1,7 +1,7 @@
-{ ... }: {
+{ config, ... }: {
   home.file.aerospace = {
     enable = true;
     target = "./.config/aerospace/aerospace.toml";
-    source = ./aerospace.toml;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixconfig/applications/aerospace/aerospace.toml";
   };
 }
