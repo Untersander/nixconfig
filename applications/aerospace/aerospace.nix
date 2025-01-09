6c1eb34eec8eb0f34f75590f0b2ff45742cab8d2
Jan Untersander <jan.untersander@ost.ch>
2025-01-09 05:31:15 +0100

    Add docs to aerospace.nix

--- applications/aerospace/aerospace.nix
+++ applications/aerospace/aerospace.nix
@@ -2,6 +2,7 @@
   home.file.aerospace = {
     enable = true;
     target = "./.config/aerospace/aerospace.toml";
+    # Out of store symlink to the actual file in ~/nixconfig to remove the need to rebuild the system
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixconfig/applications/aerospace/aerospace.toml";
   };
 }
