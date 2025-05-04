4969d47ad583735cf4f231034be27447ac3c8b37
Jan Untersander <jan.untersander@ost.ch>
2025-05-04 22:57:39 +0200

    Fix gobin/path bug

--- applications/go.nix
+++ applications/go.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.go = {
     enable = true;
     package = pkgs.go_1_24;
-    goPath = "${config.home.homeDirectory}/go";
-    goBin = "${config.home.homeDirectory}/go/bin";
+    goPath = "/go";
+    goBin = "/go/bin";
   };
 }
